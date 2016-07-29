44ed888d5ff22a42865cc02c6e0a957a98437167
James Johnson <d0c.s4vage@gmail.com>
2016-07-29 00:30:41 -0500

    due_on and start_on with tasks are now the correct type

--- pyactlab/models.py
+++ pyactlab/models.py
@@ -401,8 +401,8 @@ class Task(Model, Taskable, Attachable, Commentable, Completable):
         "assignee_id":        int,    # (integer) - The user assigned and responsible for the Task.
         "other_assignees":    list,    # (array) - The people assigned to the Task.
         # TODO
-        "due_on":            unicode,    # (date) - The task due date.
-        "start_on":          unicode,
+        "due_on":            int,    # (date) - The task due date.
+        "start_on":          int,
         "created_by_id":    int,    # (integer) - Use for a known user who already has an account in the system.
         "created_by_name":    unicode,    # (string) - Use for anonymous user, who don't have an account in the system (can not be used with created_by_id).
         "created_by_email":    unicode    # (string) - Used for anonymous users.
